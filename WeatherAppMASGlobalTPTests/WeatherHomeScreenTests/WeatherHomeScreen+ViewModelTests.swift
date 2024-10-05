a9077671383c82c067d5c8c86ebe3a81e975786b
Gokul <gokulplkl@gmail.com>
2024-10-05 15:34:28 -0400

    hot fixes

--- WeatherAppMASGlobalTPTests/WeatherHomeScreenTests/WeatherHomeScreen+ViewModelTests.swift
+++ WeatherAppMASGlobalTPTests/WeatherHomeScreenTests/WeatherHomeScreen+ViewModelTests.swift
@@ -10,23 +10,23 @@ import Testing
 
 struct WeatherHomeScreen_ViewModelTests {
 
-    @Test func cityUpdateOnLocationFromLocationManagerTest() async throws {
-        
+    @Test
+    func cityUpdateOnLocationFromLocationManagerTest() async throws {
         let city = City(name: "Buffalo", state: "NY", country: "US")
         let sut = WeatherHomeScreen.ViewModel(city: city)
-        
+
         let newlyUpdatedCity = City(name: "New York City", state: "NY", country: "US")
-        
+
         sut.updateLastUpdatedLocationCity(city: newlyUpdatedCity)
-        
+
         // update with core location not enabled
         #expect(sut.city != newlyUpdatedCity)
-        
+
         sut.shouldUpdateWithCoreLocation = true
-        
+
+        // update with core location enabled
         sut.updateLastUpdatedLocationCity(city: newlyUpdatedCity)
         #expect(sut.city == newlyUpdatedCity)
-        
     }
 
 }
